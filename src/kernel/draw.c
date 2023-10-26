9ba39bb673afc3bd9fdd649f1de99ebf90ac670b
Szymon Miśtal <mistalzaq@gmail.com>
2023-10-26 19:18:39 +0200

    Add `clear_screen` method

--- src/kernel/draw.c
+++ src/kernel/draw.c
@@ -6,7 +6,7 @@ void draw_rect(int x, int y, int w, int h)
     {
         for (int j = 0; j < w; j++)
         {
-            VGA_BUFFER[(320 * (i + y)) + (j + x)] = 0x0F;
+            VGA_BUFFER[(SCREEN_WIDTH * (i + y)) + (j + x)] = 0x0F;
         }
     }
 }
@@ -17,7 +17,18 @@ void draw_img(char *img, int x, int y, int w, int h)
     {
         if (img[i] == 1)
         {
-            VGA_BUFFER[(320 * ((i / w) + y)) + ((i % w) + x)] = 0x0F;
+            VGA_BUFFER[(SCREEN_WIDTH * ((i / w) + y)) + ((i % w) + x)] = 0x0F;
+        }
+    }
+}
+
+void clear_screen() 
+{
+    for (int i = 0; i < SCREEN_HEIGHT; i++)
+    {
+        for (int j = 0; j < SCREEN_WIDTH; j++)
+        {
+            VGA_BUFFER[(SCREEN_WIDTH * i) + j] = 0x0;
         }
     }
 }
--- src/kernel/draw.h
+++ src/kernel/draw.h
@@ -1,4 +1,7 @@
 #define VGA_BUFFER ((char *)0xA0000)
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 200
 
 void draw_rect(int x, int y, int w, int h);
 void draw_img(char *img, int x, int y, int w, int h);
+void clear_screen();
